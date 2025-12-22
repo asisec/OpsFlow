1b42b5a16ae89fd32820245de107551453c252b0
AlptugD <2005alptu@gmail.com>
2025-12-22 16:13:39 +0300

    #deneme

--- OpsFlow/UI/Forms/LoginForm.cs
+++ OpsFlow/UI/Forms/LoginForm.cs
@@ -9,5 +9,10 @@ namespace OpsFlow.UI.Forms
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("merhaba");
+        }
     }
 }
