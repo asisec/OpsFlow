942a393466f7af0e46b48bb13371d1f58c4629a9
alisec <alisahinpisirici@icloud.com>
2025-12-22 15:30:57 +0300

    feat: initial project setup and source code restoration

---
+++ OpsFlow/Program.cs
@@ -0,0 +1,14 @@
+using OpsFlow.UI.Forms;
+
+namespace OpsFlow
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new LoginForm());
+        }
+    }
+}
