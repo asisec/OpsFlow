c413531f61d779b6862155eaf41bb07394edd758
AlptugD <2005alptu@gmail.com>
2026-01-03 04:04:52 +0300

    fix(ui): correct CompanyRegisterForm popup positioning and overlay alignment

--- OpsFlow/UI/Forms/Management/PersonelCard.cs
+++ OpsFlow/UI/Forms/Management/PersonelCard.cs
@@ -14,4 +14,26 @@ public partial class PersonelCard : UserControl
     {
         InitializeComponent();
     }
+
+    private void btnViewProfile_Click(object sender, EventArgs e)
+    {
+        if (this.FindForm() is Form anaForm)
+        {
+            Form dimmer = new Form();
+            dimmer.FormBorderStyle = FormBorderStyle.None;
+            dimmer.BackColor = Color.Black;
+            dimmer.Opacity = 0.60d;
+            dimmer.ShowInTaskbar = false;
+            dimmer.StartPosition = FormStartPosition.Manual;
+            dimmer.Location = anaForm.Location;
+            dimmer.Size = anaForm.Size;
+            using (ProfileEditForm popup = new ProfileEditForm())
+            {
+                popup.StartPosition = FormStartPosition.CenterParent;
+                dimmer.Show(anaForm);
+                popup.ShowDialog(dimmer);
+                dimmer.Close();
+            }
+        }
+    }
 }
