eed8fffee4be861c9ede0cac0766f83c96226c28
AlptugD <2005alptu@gmail.com>
2026-01-05 21:58:13 +0300

    feat(ui): implement settings layout and profile management view

---
+++ OpsFlow/UI/Forms/Management/ProfileControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpsFlow.UI.Forms.Management;
+
+public partial class ProfileControl : UserControl
+{
+    public ProfileControl()
+    {
+        InitializeComponent();
+    }
+
+    private void txtName_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+}
---
+++ OpsFlow/UI/Forms/Management/SettingsForm.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace OpsFlow.UI.Forms.Management;
+
+partial class SettingsForm
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges25 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges26 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges27 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges28 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+        pnlSettingsMenu = new Guna.UI2.WinForms.Guna2Panel();
+        btnGeneral = new Guna.UI2.WinForms.Guna2Button();
+        btnProfile = new Guna.UI2.WinForms.Guna2Button();
+        btnNotifications = new Guna.UI2.WinForms.Guna2Button();
+        btnFax = new Guna.UI2.WinForms.Guna2Button();
+        btnSecurity = new Guna.UI2.WinForms.Guna2Button();
+        pnlSettingsContent = new Guna.UI2.WinForms.Guna2Panel();
+        pnlSettingsMenu.SuspendLayout();
+        SuspendLayout();
+        // 
+        // pnlSettingsMenu
+        // 
+        pnlSettingsMenu.Controls.Add(btnSecurity);
+        pnlSettingsMenu.Controls.Add(btnFax);
+        pnlSettingsMenu.Controls.Add(btnNotifications);
+        pnlSettingsMenu.Controls.Add(btnProfile);
+        pnlSettingsMenu.Controls.Add(btnGeneral);
+        pnlSettingsMenu.CustomizableEdges = customizableEdges25;
+        pnlSettingsMenu.Dock = DockStyle.Left;
+        pnlSettingsMenu.Location = new Point(0, 0);
+        pnlSettingsMenu.Name = "pnlSettingsMenu";
+        pnlSettingsMenu.ShadowDecoration.CustomizableEdges = customizableEdges26;
+        pnlSettingsMenu.Size = new Size(250, 791);
+        pnlSettingsMenu.TabIndex = 0;
+        // 
+        // btnGeneral
+        // 
+        btnGeneral.CustomizableEdges = customizableEdges23;
+        btnGeneral.DisabledState.BorderColor = Color.DarkGray;
+        btnGeneral.DisabledState.CustomBorderColor = Color.DarkGray;
+        btnGeneral.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+        btnGeneral.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+        btnGeneral.FillColor = Color.Transparent;
+        btnGeneral.Font = new Font("Poppins", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+        btnGeneral.ForeColor = Color.White;
+        btnGeneral.Location = new Point(1, 0);
+        btnGeneral.Name = "btnGeneral";
+        btnGeneral.ShadowDecoration.CustomizableEdges = customizableEdges24;
+        btnGeneral.Size = new Size(247, 56);
+        btnGeneral.TabIndex = 0;
+        btnGeneral.Text = "Genel";
+        // 
+        // btnProfile
+        // 
+        btnProfile.CustomizableEdges = customizableEdges21;
+        btnProfile.DisabledState.BorderColor = Color.DarkGray;
+        btnProfile.DisabledState.CustomBorderColor = Color.DarkGray;
+        btnProfile.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+        btnProfile.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+        btnProfile.FillColor = Color.Transparent;
+        btnProfile.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+        btnProfile.ForeColor = Color.White;
+        btnProfile.Location = new Point(1, 61);
+        btnProfile.Name = "btnProfile";
+        btnProfile.ShadowDecoration.CustomizableEdges = customizableEdges22;
+        btnProfile.Size = new Size(247, 56);
+        btnProfile.TabIndex = 1;
+        btnProfile.Text = "Profil";
+        // 
+        // btnNotifications
+        // 
+        btnNotifications.CustomizableEdges = customizableEdges19;
+        btnNotifications.DisabledState.BorderColor = Color.DarkGray;
+        btnNotifications.DisabledState.CustomBorderColor = Color.DarkGray;
+        btnNotifications.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+        btnNotifications.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+        btnNotifications.FillColor = Color.Transparent;
+        btnNotifications.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+        btnNotifications.ForeColor = Color.White;
+        btnNotifications.Location = new Point(1, 122);
+        btnNotifications.Name = "btnNotifications";
+        btnNotifications.ShadowDecoration.CustomizableEdges = customizableEdges20;
+        btnNotifications.Size = new Size(247, 56);
+        btnNotifications.TabIndex = 2;
+        btnNotifications.Text = "Bildirimler";
+        // 
+        // btnFax
+        // 
+        btnFax.CustomizableEdges = customizableEdges17;
+        btnFax.DisabledState.BorderColor = Color.DarkGray;
+        btnFax.DisabledState.CustomBorderColor = Color.DarkGray;
+        btnFax.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+        btnFax.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+        btnFax.FillColor = Color.Transparent;
+        btnFax.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+        btnFax.ForeColor = Color.White;
+        btnFax.Location = new Point(1, 184);
+        btnFax.Name = "btnFax";
+        btnFax.ShadowDecoration.CustomizableEdges = customizableEdges18;
+        btnFax.Size = new Size(247, 56);
+        btnFax.TabIndex = 3;
+        btnFax.Text = "Fatura";
+        // 
+        // btnSecurity
+        // 
+        btnSecurity.CustomizableEdges = customizableEdges15;
+        btnSecurity.DisabledState.BorderColor = Color.DarkGray;
+        btnSecurity.DisabledState.CustomBorderColor = Color.DarkGray;
+        btnSecurity.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+        btnSecurity.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+        btnSecurity.FillColor = Color.Transparent;
+        btnSecurity.Font = new Font("Poppins Medium", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+        btnSecurity.ForeColor = Color.White;
+        btnSecurity.Location = new Point(1, 245);
+        btnSecurity.Name = "btnSecurity";
+        btnSecurity.ShadowDecoration.CustomizableEdges = customizableEdges16;
+        btnSecurity.Size = new Size(247, 56);
+        btnSecurity.TabIndex = 4;
+        btnSecurity.Text = "Güvenlik";
+        // 
+        // pnlSettingsContent
+        // 
+        pnlSettingsContent.CustomizableEdges = customizableEdges27;
+        pnlSettingsContent.Dock = DockStyle.Fill;
+        pnlSettingsContent.Location = new Point(250, 0);
+        pnlSettingsContent.Name = "pnlSettingsContent";
+        pnlSettingsContent.ShadowDecoration.CustomizableEdges = customizableEdges28;
+        pnlSettingsContent.Size = new Size(750, 791);
+        pnlSettingsContent.TabIndex = 1;
+        // 
+        // SettingsForm
+        // 
+        AutoScaleDimensions = new SizeF(8F, 20F);
+        AutoScaleMode = AutoScaleMode.Font;
+        BackColor = Color.FromArgb(16, 18, 24);
+        ClientSize = new Size(1000, 791);
+        Controls.Add(pnlSettingsContent);
+        Controls.Add(pnlSettingsMenu);
+        FormBorderStyle = FormBorderStyle.None;
+        Name = "SettingsForm";
+        Text = "SettingsForm";
+        pnlSettingsMenu.ResumeLayout(false);
+        ResumeLayout(false);
+    }
+
+    #endregion
+
+    private Guna.UI2.WinForms.Guna2Panel pnlSettingsMenu;
+    private Guna.UI2.WinForms.Guna2Button btnSecurity;
+    private Guna.UI2.WinForms.Guna2Button btnFax;
+    private Guna.UI2.WinForms.Guna2Button btnNotifications;
+    private Guna.UI2.WinForms.Guna2Button btnProfile;
+    private Guna.UI2.WinForms.Guna2Button btnGeneral;
+    private Guna.UI2.WinForms.Guna2Panel pnlSettingsContent;
+}
